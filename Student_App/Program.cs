9ac56776230781f61e5ffe8086851a868a086ba4
sanket..ghorpade <sanket..ghorpade@HP2335>
2025-06-25 20:13:17 +0530

    Add project files.

---
+++ Student_App/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Student_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //object creation using default constructor
+            Student s1 = new Student();
+
+            //object creation with negative roll no
+            try
+            {
+                Student s2 = new Student(-1, "Rakesh", 9.6);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine("Object Creation failed!!");
+            }
+
+            //object creation with invalid cgpa 
+            try
+            {
+                Student s3 = new Student(1, "Rakesh", 11.2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nError: {ex.Message}");
+                Console.WriteLine("Object Creation failed!!");
+            }
+
+            //object creation with name with length less than 5
+            Student s4 = new Student(1, "jai", 7.4);
+            Console.WriteLine("\nOutput from object with name.Length < 5: ");
+            Console.WriteLine($"Name: {s4.Name}");
+
+            //object creation with all valid paramters
+            Student s5 = new Student(2, "Sanket", 9.7);
+            Console.WriteLine("\nOutput from object with name.Length > 5: ");
+            Console.WriteLine($"Percentage of {s5.Name}: {s5.CalculatePercentage()}");
+        }
+    }
+}
---
+++ Student_App/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_App
+{
+    internal class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public double CGPA { get; set; }
+
+        private double percentage;
+
+        // Default Constructor
+        public Student() { }
+
+        //Parameterised Constructor
+        public Student(int rollNo, string name, double cgpa)
+        {
+            if(rollNo < 0) { throw new ArgumentOutOfRangeException(nameof(rollNo), "Roll no should be positive"); }
+            if(cgpa < 1 || cgpa > 10) { throw new ArgumentOutOfRangeException(nameof(cgpa), "CGPA should be in the range 1-10"); }
+            RollNo = rollNo;
+            CGPA = cgpa;
+            if (name.Length < 5) Name = "";
+            else Name = name;
+        }
+
+        public double CalculatePercentage()
+        {
+            return percentage = Math.Round(7.1 * CGPA + 11,2);
+        }
+    }
+}
